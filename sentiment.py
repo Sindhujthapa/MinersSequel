9d7608349f5cb4eab89598048b4ab52f560414e6
MariiaOleksiienko <mariiaoleksiienko@utexas.edu>
2025-05-04 02:00:06 +0300

    sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -38,7 +38,10 @@ X = vectorizer.fit_transform(df['review'].astype(str))
 y = df['voted_up']
 
 # 5. Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
 
 # 6. Decision Tree
 dt = DecisionTreeClassifier(random_state=42)
